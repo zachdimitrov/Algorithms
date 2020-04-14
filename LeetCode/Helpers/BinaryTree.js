601552abb3cc99a8443e09327d5ffd52607d617b
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2020-04-14 17:19:07 +0300

    binary tree

---
+++ LeetCode/Helpers/BinaryTree.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        var newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        
+    }
+}
