d4a651af210659b689b1a8b3fbb8f32e069faa4b
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2020-04-15 03:56:27 +0300

    solve the binary array problem

--- LeetCode/Week-2_8-14.04/ContiguousArray.js
+++ LeetCode/Week-2_8-14.04/ContiguousArray.js
@@ -5,7 +5,27 @@
  * @return {number}
  */
 var findMaxLength = function(nums) {
+    let maxCount = 0;
+    let test = 0;
+    let hm = [];
 
+    for (let j = 0; j < nums.length; j++) {
+        let e = nums[j];
+        if (e === 0) e = -1;
+        test += e;
+
+        if (test === 0) {
+            maxCount = j + 1;
+        }
+
+        if (hm[test] != undefined) {
+            maxCount = Math.max(j - hm[test], maxCount);
+        } else {
+            hm[test] = j;
+        }
+    }
+
+    return maxCount;
 };
 
-console.log(findMaxLength([1, 2, 3, 1, 0, 1, 0, 1, 2, 3, 1, 0, 1]));
+console.log(findMaxLength([0, 0, 1, 0, 0, 0, 1, 1, 1]));
